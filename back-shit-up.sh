d8cd556f86c2dfb0ccc2f7e7ec82bfcf625a61a0
Vladimir Grechka <vladimir.grechka@gmail.com>
2017-04-04 20:04:05 +0300

    No fucking comments

---
+++ back-shit-up.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+stamp=20170404
+
+outdir=/mnt/e/bak/$stamp
+echo Backing shit up...
+
+rm -rf $outdir
+mkdir $outdir
+
+cd /mnt/e/fegh
+zip -r $outdir/fe-$stamp.zip . \
+    -x /.git* \
+    -x /node_modules/* \
+    -x /composer/vendor/* \
+    -x /out/* \
+    -x /lib-gradle/* \
+    -x /.gradle/*
+
+cd /mnt/e/work/aps
+zip -r $outdir/aps-$stamp.zip . \
+    -x /.git* \
+    -x /node_modules/* \
+    -x /back/.gradle/* \
+    -x /back/built/* \
+    -x /back/lib-gradle/* \
+    -x /back/out/* \
+    -x /front/out/* \
+    -x /javaagent/out/* \
+    -x /kotlin-js-playground/node_modules/* \
+    -x /kotlin-js-playground/out/* \
+    -x /kotlin-jvm-playground/out/* \
+    -x /tools/out/*
+
+echo OK
+
