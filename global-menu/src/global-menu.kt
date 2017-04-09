b4b4ac2a1227c70453b7f10cecc1168c042470ef
Vladimir Grechka <vladimir.grechka@gmail.com>
2017-04-09 23:54:29 +0300

    No fucking comments

--- global-menu/src/global-menu.kt
+++ global-menu/src/global-menu.kt
@@ -1,20 +1,41 @@
 package vgrechka.globalmenu
 
+import com.sun.jna.platform.win32.User32
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.collections.FXCollections
+import javafx.concurrent.Task
 import javafx.scene.Scene
 import javafx.scene.control.*
 import javafx.scene.control.Alert.AlertType
-import javafx.scene.input.Clipboard
-import javafx.scene.input.DataFormat
-import javafx.scene.input.KeyCode
-import javafx.scene.input.KeyEvent
+import javafx.scene.input.*
 import javafx.scene.layout.VBox
 import javafx.stage.Stage
+import javafx.stage.StageStyle
+import javafx.stage.WindowEvent
+import org.jnativehook.GlobalScreen
+import org.jnativehook.mouse.NativeMouseAdapter
+import org.jnativehook.mouse.NativeMouseEvent
 import vgrechka.*
 import vgrechka.idea.hripos.*
 import vgrechka.ideabackdoor.*
+import java.awt.Robot
+import java.util.concurrent.ExecutorService
+import java.util.logging.Level
+import java.util.logging.Logger
+import kotlin.properties.Delegates.notNull
 import kotlin.system.exitProcess
+import java.lang.reflect.Array.setShort
+import java.lang.reflect.AccessibleObject.setAccessible
+import org.jnativehook.NativeInputEvent
+import org.jnativehook.keyboard.NativeKeyAdapter
+import org.jnativehook.keyboard.NativeKeyEvent
+import java.awt.MouseInfo
+import java.util.concurrent.TimeUnit
+import java.util.ArrayList
+import java.util.concurrent.AbstractExecutorService
+import kotlin.concurrent.thread
+
 
 object JFXStuff {
     fun infoAlert(headerText: String) {
@@ -40,90 +61,199 @@ object JFXStuff {
 }
 
 interface MenuItem {
-    val control: Control?
-    fun onEnterKey()
+    fun makeDetailsControl(): Control?
+    fun run()
 }
 
 object FuckItem : MenuItem {
     override fun toString() = "Fuck"
-    override val control = Label("I am the Fuck item")
+    override fun makeDetailsControl() = Label("I am the Fuck item")
 
-    override fun onEnterKey() {
+    override fun run() {
         JFXStuff.infoAlert("Fuck, fuck, fuck...")
     }
 }
 
 object ShitItem : MenuItem {
     override fun toString() = "Shit"
-    override val control = null
+    override fun makeDetailsControl() = null
 
-    override fun onEnterKey() {
+    override fun run() {
         JFXStuff.errorAlert("Shit? What shit?", "No shit for you today...")
     }
 }
 
 object BitchItem : MenuItem {
     override fun toString() = "Bitch"
-    override val control = Label("I am the Bitch item")
+    override fun makeDetailsControl() = Label("I am the Bitch item")
 
-    override fun onEnterKey() {
+    override fun run() {
         JFXStuff.errorAlert("Fuck you. Just fuck you")
     }
 }
 
 object GlobalMenuItem_Phizdets_MakeSenseOfPHPSpew : MenuItem {
-    val spew = run {
-        Clipboard.getSystemClipboard().getContent(DataFormat.PLAIN_TEXT)?.toString()
-            ?: "Nothing interesting in clipboard"
-    }
+    var spew by notNull<String>()
 
     override fun toString() = "Phizdets: Make sense of PHP spew"
 
-    override val control = TextArea()-{o->
+    override fun makeDetailsControl() = TextArea()-{o->
         o.minHeight = 100.0
-        o.text = spew
+        o.text = ""
+
+        thread {
+            Thread.sleep(250)
+            Platform.runLater {
+                spew = Clipboard.getSystemClipboard().getContent(DataFormat.PLAIN_TEXT)?.toString()
+                    ?: "Nothing interesting in clipboard"
+                o.text = spew
+            }
+        }
     }
 
-    override fun onEnterKey() {
+    override fun run() {
         try {
             sendCommandToIDEABackdoor(Command_PhiMakeSenseOfPHPSpew(spew))
-            exitProcess(0)
+            GlobalMenuGlobal.primaryStage.isIconified = true
         } catch(e: Exception) {
             JFXStuff.errorAlert(e)
         }
     }
 }
 
+object GlobalMenuGlobal {
+    var primaryStage by notNull<Stage>()
+}
+
+private class VoidDispatchService : AbstractExecutorService() {
+    private var running = false
+
+    init {
+        running = true
+    }
+
+    override fun shutdown() {
+        running = false
+    }
+
+    override fun shutdownNow(): List<Runnable> {
+        running = false
+        return ArrayList(0)
+    }
+
+    override fun isShutdown(): Boolean {
+        return !running
+    }
+
+    override fun isTerminated(): Boolean {
+        return !running
+    }
+
+    @Throws(InterruptedException::class)
+    override fun awaitTermination(timeout: Long, unit: TimeUnit): Boolean {
+        return true
+    }
+
+    override fun execute(r: Runnable) {
+        r.run()
+    }
+}
+
 class StartGlobalMenu : Application() {
+    var vbox by notNull<VBox>()
+    var listView by notNull<ListView<MenuItem>>()
+
+    fun syncSelectedItem() {
+        vbox.children.remove(1, vbox.children.size)
+        val item = listView.selectionModel.selectedItem
+        if (item != null) {
+            item.makeDetailsControl()?.let {vbox.children += it}
+        }
+    }
+
     override fun start(primaryStage: Stage) {
-        val vbox = VBox()
-        val listView = ListView<MenuItem>(FXCollections.observableArrayList(
+        GlobalMenuGlobal.primaryStage = primaryStage
+        val robot = Robot()
+
+        run { // Install hook
+            val logger = Logger.getLogger(GlobalScreen::class.java.`package`.name)
+            logger.level = Level.WARNING
+            logger.useParentHandlers = false
+
+//            GlobalScreen.setEventDispatcher(VoidDispatchService())
+            GlobalScreen.registerNativeHook()
+
+            GlobalScreen.addNativeKeyListener(object:NativeKeyAdapter() {
+                override fun nativeKeyPressed(e: NativeKeyEvent) {
+                    try {
+                        if (e.modifiers.and(NativeMouseEvent.CTRL_L_MASK) == NativeMouseEvent.CTRL_L_MASK) {
+                            if (e.keyCode == NativeKeyEvent.VC_1) {
+                                try {
+//                                    robot.keyPress(java.awt.event.KeyEvent.VK_CONTROL)
+                                    robot.keyPress('C'.toInt())
+                                    // clog("Copied to clipboard")
+                                } finally {
+                                    robot.keyRelease('C'.toInt())
+//                                    robot.keyRelease(java.awt.event.KeyEvent.VK_CONTROL)
+                                }
+
+                                Platform.runLater {
+                                    try {
+//                                        val loc = MouseInfo.getPointerInfo().location
+//                                        primaryStage.x = loc.x.toDouble()
+//                                        primaryStage.y = loc.y.toDouble()
+                                        primaryStage.isIconified = true
+                                        primaryStage.isIconified = false
+                                        syncSelectedItem()
+                                    } catch(e: Exception) {
+                                        e.printStackTrace()
+                                    }
+                                }
+                            }
+                        }
+                    } catch(e: Throwable) {
+                        e.printStackTrace()
+                    }
+                }
+            })
+        }
+
+        primaryStage.addEventHandler(WindowEvent.WINDOW_HIDDEN) {e->
+            exitProcess(0)
+        }
+
+        vbox = VBox()
+        listView = ListView<MenuItem>(FXCollections.observableArrayList(
             GlobalMenuItem_Phizdets_MakeSenseOfPHPSpew,
             FuckItem,
             ShitItem,
             BitchItem))
         vbox.children += listView
-        listView.selectionModel.selectedItemProperty().addListener {observable, oldValue, newValue ->
-            vbox.children.remove(1, vbox.children.size)
-            newValue.control?.let {vbox.children += it}
 
+        listView.selectionModel.selectedItemProperty().addListener {observable, oldValue, newValue ->
+            syncSelectedItem()
         }
+
         listView.selectionModel.selectFirst()
         listView.addEventHandler(KeyEvent.KEY_PRESSED) {e->
             if (e.code == KeyCode.ENTER) {
-                val item = listView.selectionModel.selectedItem
-                item.onEnterKey()
+                listView.selectionModel.selectedItem.run()
             }
             else if (e.code == KeyCode.ESCAPE) {
-                primaryStage.close()
+                primaryStage.isIconified = true
+            }
+        }
+        listView.addEventHandler(MouseEvent.MOUSE_CLICKED) {e->
+            if (e.clickCount == 2) {
+                listView.selectionModel.selectedItem.run()
             }
         }
 
         val scene = Scene(vbox, 300.0, 250.0)
 
-        primaryStage.modality
         primaryStage.title = "Global Menu"
         primaryStage.scene = scene
+        primaryStage.initStyle(StageStyle.DECORATED)
         primaryStage.show()
     }
 
