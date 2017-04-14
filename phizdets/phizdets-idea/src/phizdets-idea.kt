8e74c8faea5adbc8562d8176ad3cc0f3d66dea47
Vladimir Grechka <vladimir.grechka@gmail.com>
2017-04-14 16:23:32 +0300

    No fucking comments

--- phizdets/phizdets-idea/src/phizdets-idea.kt
+++ phizdets/phizdets-idea/src/phizdets-idea.kt
@@ -6,6 +6,7 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.components.ApplicationComponent
 import com.intellij.openapi.options.ex.SingleConfigurableEditor
 import com.intellij.openapi.options.newEditor.SettingsDialog
@@ -18,7 +19,9 @@ import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil
 import com.intellij.openapi.roots.ui.configuration.ProjectStructureConfigurable
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
+import com.intellij.util.PlatformUtils
 import com.intellij.util.lang.UrlClassLoader
+import com.sun.jna.platform.win32.User32
 import vgrechka.*
 import vgrechka.idea.*
 import java.io.File
@@ -51,36 +54,126 @@ class PhizdetsIDEAPlugin : ApplicationComponent {
 
         run {
             val action = object : AnAction("Phizdets: _Mess Around") {
+                val robot = Robot()
+
                 override fun actionPerformed(event: AnActionEvent) {
+//                    testAddSDK()
+                    testCreateRunConfiguration()
+                }
+
+                fun key(code: Int) {
+                    robot.keyPress(code)
+                    robot.keyRelease(code)
+                }
+
+                fun key(c: Char) {
+                    key(c.toUpperCase().toInt())
+                }
+
+                fun click() {
+                    Thread.sleep(500)
+                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
+                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
+                    Thread.sleep(500)
+                }
+
+                private fun testCreateRunConfiguration() {
+                    bitchingThread {
+                        robot.keyPress(KeyEvent.VK_ALT)
+                        key('u')
+                        robot.keyRelease(KeyEvent.VK_ALT)
+                        key('r')
+
+                        waitForFuckingWindow("Run/Debug Configurations")
+
+                        click(318, 158)
+                        click(372, 480)
+                        click(1000, 375)
+                        click(1000, 430)
+                        click(944, 231)
+                        typeText("E:\\fegh\\aps\\aps-back-phi\\out\\production\\aps-back-phi\\aps-back-phi.php")
+
+                        click(880, 153)
+                        repeat(15) {key(KeyEvent.VK_BACK_SPACE)}
+                        typeText("fuck")
+
+                        key(KeyEvent.VK_ENTER)
+                    }
+                }
+
+                private fun typeText(text: String) {
+                    text.forEach {c->
+                        var holdShift = c.isLetter() && c.isUpperCase()
+
+                        val keyCode = when {
+                            c.isLetterOrDigit() -> c.toUpperCase().toInt()
+                            else -> when (c) {
+                                '-' -> KeyEvent.VK_MINUS
+                                ' ' -> KeyEvent.VK_SPACE
+                                ':' -> {holdShift = true; KeyEvent.VK_SEMICOLON}
+                                '.' -> KeyEvent.VK_PERIOD
+                                '/' -> KeyEvent.VK_SLASH
+                                '\\' -> KeyEvent.VK_BACK_SLASH
+                                else -> wtf("Dunno how to type key `$c`")
+                            }
+                        }
+
+                        if (holdShift)
+                            robot.keyPress(KeyEvent.VK_SHIFT)
+
+                        try {
+                            robot.keyPress(keyCode)
+                            robot.keyRelease(keyCode)
+                        } finally {
+                            if (holdShift)
+                                robot.keyRelease(KeyEvent.VK_SHIFT)
+                        }
+                    }
+                }
+
+                private fun bitchingThread(fuck: () -> Unit) {
                     thread {
-                        val robot = Robot()
-                        val origLocation = MouseInfo.getPointerInfo().location
-
-                        fun key(code: Int) {
-                            robot.keyPress(code)
-                            robot.keyRelease(code)
+                        try {
+                            fuck()
+                        } catch(e: Throwable) {
+                            ApplicationManager.getApplication().invokeLater {
+                                Messages.showErrorDialog(e.message, "No Way")
+                            }
                         }
+                    }
+                }
 
-                        fun click() {
-                            Thread.sleep(500)
-                            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
-                            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
-                            Thread.sleep(500)
+                private fun waitForFuckingWindow(title: String) {
+                    var gotWindow = false
+                    for (i in 1..3) {
+                        val hwnd = User32.INSTANCE.FindWindow(null, title)
+                        if (hwnd != null) {
+                            gotWindow = true
+                            break
                         }
+                        Thread.sleep(500)
+                    }
+                    if (!gotWindow)
+                        bitch("No fucking window titled `$title`")
+                }
 
+                private fun click(x: Int, y: Int) {
+                    robot.mouseMove(x, y)
+                    click()
+                }
+
+                private fun testAddSDK() {
+                    bitchingThread {
                         robot.keyPress(KeyEvent.VK_CONTROL)
                         robot.keyPress(KeyEvent.VK_ALT)
                         robot.keyPress(KeyEvent.VK_SHIFT)
-                        key('S'.toInt())
+                        key('s')
                         robot.keyRelease(KeyEvent.VK_SHIFT)
                         robot.keyRelease(KeyEvent.VK_ALT)
                         robot.keyRelease(KeyEvent.VK_CONTROL)
 
-                        robot.mouseMove(334, 314)
-                        click()
-
-                        robot.mouseMove(480, 90)
-                        click()
+                        click(334, 314)
+                        click(480, 90)
 
                         key(KeyEvent.VK_DOWN)
                         Thread.sleep(250)
@@ -88,16 +181,8 @@ class PhizdetsIDEAPlugin : ApplicationComponent {
                         Thread.sleep(250)
                         key(KeyEvent.VK_ENTER)
                     }
-
-//                    object : SingleConfigurableEditor(event.project, ProjectStructureConfigurable.getInstance(event.project), SettingsDialog.DIMENSION_KEY) {
-//                        override fun getStyle(): DialogWrapper.DialogStyle {
-//                            return DialogWrapper.DialogStyle.COMPACT
-//                        }
-//                    }.show()
-
-
-//                    Messages.showInfoMessage("Fuck you", "aaaaaa")
                 }
+
             }
             group.add(action)
         }
