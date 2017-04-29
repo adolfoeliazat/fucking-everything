b0018faefa958f37d3e23f6ec125503b0ad2ddfb
Vladimir Grechka <vladimir.grechka@gmail.com>
2017-04-29 09:10:38 +0300

    No fucking comments

--- okaml/okaml-fucking/2/fuck2.ml
+++ okaml/okaml-fucking/2/fuck2.ml
@@ -3,12 +3,41 @@ open Core.Std
 let println s = print_string (s ^ "\n")
 
 module Pizda = struct
+    module Fuck = struct
+        type t = {value : string}
+    end
+
+    module Shit = struct
+        type t = {value : string(*; pizda: Pizda.t*)}
+    end
+
     type t =
     | Fuck of Fuck.t
     | Shit of Shit.t
+
+    let evaluate p =
+        match p with
+        | Fuck p -> "some fuck" ^ p.value
+        | Shit p -> "some shit " ^ p.value
 end
 
 
 let () =
-    println "I am the fuck2"
+    println "I am the fuck2";
+    Pizda.Shit {value = "big"} |> Pizda.evaluate |> println
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
